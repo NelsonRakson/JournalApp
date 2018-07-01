187fe9d12e149d42df238035cc28c91b1c88314a
Nelson Rakson <nelsonrakson@gmail.com>
2018-07-01 18:58:45 +0100

    Added time strip and splash screen

--- app/src/main/java/com/example/journal/journalapp/LauncherActivity.java
+++ app/src/main/java/com/example/journal/journalapp/LauncherActivity.java
@@ -2,10 +2,8 @@ package com.example.journal.journalapp;
 
 import android.content.Intent;
 import android.os.CountDownTimer;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
 
 public class LauncherActivity extends AppCompatActivity {
 
@@ -13,12 +11,6 @@ public class LauncherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
-
-        ActionBar actionBar =getSupportActionBar();
-        if(actionBar!=null){
-            getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
-            actionBar.hide();
-        }
 
         new CountDownTimer(3000,3000){
 
